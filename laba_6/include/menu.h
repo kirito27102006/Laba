e1a6addf9f964c686c936203d07193d3f6d32f32
Andrey <andrejignatuk372@gmail.com>
2025-10-23 21:24:25 +0300

    j

--- laba_6/include/menu.h
+++ laba_6/include/menu.h
@@ -6,7 +6,7 @@ class MyException  : public std::exception  {
 private:
     std::string message;
 public:
-    explicit MyException(const std::string& msg) : message(msg) {}
+    explicit MyException(const std::string_view& msg) : message(msg) {}
 
     const char* what() const noexcept override {
         return message.c_str();
